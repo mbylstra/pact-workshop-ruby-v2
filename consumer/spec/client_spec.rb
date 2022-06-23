e449b119b10364b860137917097364856806d605
Michael Bylstra <michael.bylstra@cultureamp.com>
2022-06-23 20:02:18 +1000

    change valid_date to date

--- consumer/spec/client_spec.rb
+++ consumer/spec/client_spec.rb
@@ -6,7 +6,7 @@ describe Client do
   let(:json_data) do
     {
       "test" => "NO",
-      "valid_date" => "2013-08-16T15:31:20+10:00",
+      "date" => "2013-08-16T15:31:20+10:00",
       "count" => 100
     }
   end
@@ -14,7 +14,7 @@ describe Client do
 
   it 'can process the json payload from the provider' do
     allow(HTTParty).to receive(:get).and_return(response)
-    expect(subject.process_data(Time.now.httpdate)).to eql([1, Time.parse(json_data['valid_date'])])
+    expect(subject.process_data(Time.now.httpdate)).to eql([1, Time.parse(json_data['date'])])
   end
 
   describe 'Pact with our provider', :pact => true do
@@ -28,13 +28,13 @@ describe Client do
       before do
         our_provider.given("data count is > 0").
           upon_receiving("a request for json data").
-          with(method: :get, path: '/provider.json', query: URI::encode('valid_date=' + date)).
+          with(method: :get, path: '/provider.json', query: URI::encode('date=' + date)).
           will_respond_with(
             status: 200,
             headers: {'Content-Type' => 'application/json'},
             body: {
               "test" => "NO",
-              "valid_date" => Pact.term(
+              "date" => Pact.term(
                   generate: "2013-08-16T15:31:20+10:00",
                   matcher: /\d{4}\-\d{2}\-\d{2}T\d{2}:\d{2}:\d{2}\+\d{2}:\d{2}/),
               "count" => Pact.like(100)
@@ -42,7 +42,7 @@ describe Client do
       end
 
       it "can process the json payload from the provider" do
-        expect(subject.process_data(date)).to eql([1, Time.parse(json_data['valid_date'])])
+        expect(subject.process_data(date)).to eql([1, Time.parse(json_data['date'])])
       end
 
     end
@@ -56,7 +56,7 @@ describe Client do
           will_respond_with(
             status: 400,
             headers: {'Content-Type' => 'application/json'},
-            body: "valid_date is required"
+            body: "date is required"
           )
         expect(subject.process_data(nil)).to eql([0, nil])
       end
@@ -64,7 +64,7 @@ describe Client do
       it "handles an invalid date parameter" do
         our_provider.given("data count is > 0").
           upon_receiving("a request with an invalid date parameter").
-          with(method: :get, path: '/provider.json', query: 'valid_date=This%20is%20not%20a%20date').
+          with(method: :get, path: '/provider.json', query: 'date=This%20is%20not%20a%20date').
           will_respond_with(
             status: 400,
             headers: {'Content-Type' => 'application/json'},
@@ -80,7 +80,7 @@ describe Client do
       it "handles the 404 response" do
         our_provider.given("data count is == 0").
           upon_receiving("a request for json data").
-          with(method: :get, path: '/provider.json', query: URI::encode('valid_date=' + date)).
+          with(method: :get, path: '/provider.json', query: URI::encode('date=' + date)).
           will_respond_with(status: 404)
         expect(subject.process_data(date)).to eql([0, nil])
       end
